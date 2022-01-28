cdecc2ba7aabcb82323e74eb1c42ee61eff7fa7e
steveng91 <steven.gasconmartin@bryanuniversity.edu>
2022-01-27 19:11:11 -0800

    week 5 update

---
+++ week6/routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const mysql = require("mysql")
+const app = express()
+
+app.get('/CreateDB', (req, res)=>{
+    let sqlString = "Create DataBase               "
+})
